1d537e8c10d44399fad45ba298ddfcc6b4408450
Leslie Liang <lliang9838@gmail.com>
2019-05-25 22:08:47 -0700

    authenticated user using bcrypt

--- blog-server/routes/login.js
+++ blog-server/routes/login.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const assert = require('assert');
+var mongoUtil = require( '../mongoUtil' );
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -13,10 +18,36 @@ router.get('/', function(req, res, next) {
 
 // POST method route
 router.post('/', function (req, res, next) {
-    console.log(req.body.username)
-    console.log(req.body.password)
-    console.log(req.body.redirect)
-    res.send('we are in POST')
+
+    let username = req.body.username
+    let plain_password = req.body.password
+    let redirect = req.body.redirect
+
+    const col = mongoUtil.db().collection('Users')
+    // console.log(mongoUtil.db())
+
+
+    col.find({"username":username.toString()}).toArray(function(err, docs) {
+        assert.equal(null, err);
+
+      if(1 !== docs.length) //cannot have more than one username
+      {
+        res.sendStatus(404);
+      }
+      else{
+        console.log(docs)
+        bcrypt.compare(plain_password, docs[0].password, function(err, result) { //reaching here means we're validated
+            console.log(result)
+            res.send('hi')
+            
+        });
+      }
+      
+    });
+
+    
+
+   res.send('we are in POST')
 });
 
 module.exports = router;
